b173fed087750209bd9d339cea9672d9b8bdea39
Damien Pageot <74134372+PageotD@users.noreply.github.com>
2021-08-05 06:14:44 +0200

    Add tests for activation functions

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -1,5 +1,5 @@
 import unittest
-from pybann.activation import sigmoid
+from pybann.activation import sigmoid, tanhyp, softplus, gaussian
 
 class test_activation(unittest.TestCase):
 
@@ -26,6 +26,78 @@ class test_activation(unittest.TestCase):
         # Output
         for i in range(len(a)):
             self.assertAlmostEqual(sigmoid(a[i], deriv=True), o[i], delta=1.e-7)
+
+    def test_tanhyp(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.23840584, 1.0, 1.76159415]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(tanhyp(a[i]), o[i], delta=1.e-7)
+
+    def test_tanhyp_derivative(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.94316265, 0.0, -2.10321397]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(tanhyp(a[i], deriv=True), o[i], delta=1.e-7)
+
+    def test_softplus(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.31326168, 0.69314718, 1.31326168]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(softplus(a[i]), o[i], delta=1.e-7)
+
+    def test_softplus_derivative(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.26894142, 0.5, 0.73105858]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(softplus(a[i], deriv=True), o[i], delta=1.e-7)
+
+    def test_gaussian(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.36787944, 1.00000000, 0.36787944]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(gaussian(a[i]), o[i], delta=1.e-7)
+
+    def test_gaussian_derivative(self):
+
+        # Input
+        a = [-1., 0., 1.]
+
+        # Attempted output
+        o = [0.73575888, 0.00000000, -0.73575888]
+
+        # Output
+        for i in range(len(a)):
+            self.assertAlmostEqual(gaussian(a[i], deriv=True), o[i], delta=1.e-7)
 
     ##### TODO
     ## tanhyp, softplus, gaussian
