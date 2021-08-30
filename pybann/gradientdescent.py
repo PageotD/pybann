8b0ff0fdd9458cd2a77d9b5c535100104341caa4
Damien Pageot <74134372+PageotD@users.noreply.github.com>
2021-08-30 15:13:56 +0200

    Flake8 pybann/gradientdescent.py

--- pybann/gradientdescent.py
+++ pybann/gradientdescent.py
@@ -1,10 +1,11 @@
 import numpy as np
-import copy as cp
 from tqdm import tqdm
+
 
 class GradientDescent:
 
-    def __init__(self, dataset, batchsize:int, alpha:float, niter:int, momentum:float, layers)->None:
+    def __init__(self, dataset, batchsize: int, alpha: float,
+                 niter: int, momentum: float, layers) -> None:
         """
         Constructor
         """
@@ -32,9 +33,9 @@ class GradientDescent:
         outValues = np.atleast_2d(dataset[1])
         return inValues, outValues
 
-    def forward(self, inValues)->list:
+    def forward(self, inValues) -> list:
         """
-        Return the output of the neural network for a given 
+        Return the output of the neural network for a given
         input dataset and stores the intermediate results
         (transfer and activation results).
 
@@ -57,48 +58,64 @@ class GradientDescent:
 
         # Feed Forward
         for ilayer in range(1, len(self.layers)):
-            transfer.append(np.dot(self.layers[ilayer].weights, activation[ilayer-1]) + self.layers[ilayer].biases)
+            transfer.append(np.dot(
+                self.layers[ilayer].weights, activation[ilayer-1])
+                + self.layers[ilayer].biases)
             activation.append(self.layers[ilayer].activation(transfer[-1]))
 
         return activation, transfer
 
-    def backward(self, activation, transfer, outValues)->None:
+    def backward(self, activation, transfer, outValues) -> None:
         """
         Calculates the gradient for each weight matrix and biase vector
         of the neural network.
         """
         # Backward
-        delta = (activation[-1] - outValues.transpose()) * self.layers[-1].activation(transfer[-1], deriv=True)
-        self.layers[-1].weightsUpdate += np.dot(delta, activation[-2].transpose())
+        delta = (activation[-1] -
+            outValues.transpose()) * self.layers[-1].activation(transfer[-1], deriv=True)
+        self.layers[-1].weightsUpdate += np.dot(
+            delta, activation[-2].transpose())
         self.layers[-1].biasesUpdate += delta
         for ilayer in range(2, len(self.layers)):
             wsvector = transfer[-ilayer]
-            delta =  self.layers[-ilayer].activation(wsvector, deriv=True) * np.dot(self.layers[-ilayer+1].weights.transpose(), delta)
-            self.layers[-ilayer].weightsUpdate += np.dot(delta, activation[-ilayer-1].transpose())
+            delta = self.layers[-ilayer].activation(
+                wsvector, deriv=True) * np.dot(
+                    self.layers[-ilayer+1].weights.transpose(), delta)
+            self.layers[-ilayer].weightsUpdate += np.dot(
+                delta, activation[-ilayer-1].transpose())
             self.layers[-ilayer].biasesUpdate += delta
 
-    def update(self)->None:
+    def update(self) -> None:
         """
         Update the weight matrix and biase vector of the neural network.
         """
         # Update weights and biases
         for ilayer in range(1, len(self.layers)):
-            
+
             # Increment
-            self.layers[ilayer].weights += (-self.alpha * self.layers[ilayer].weightsUpdate + self.momentum * self.layers[ilayer].weightsUpdateSave)
-            self.layers[ilayer].biases += (-self.alpha * self.layers[ilayer].biasesUpdate + self.momentum * self.layers[ilayer].biasesUpdateSave)
+            self.layers[ilayer].weights += (
+                -self.alpha * self.layers[ilayer].weightsUpdate
+                + self.momentum * self.layers[ilayer].weightsUpdateSave)
+            self.layers[ilayer].biases += (
+                -self.alpha * self.layers[ilayer].biasesUpdate
+                + self.momentum * self.layers[ilayer].biasesUpdateSave)
 
             # Store update
-            self.layers[ilayer].weightsUpdateSave = (-self.alpha * self.layers[ilayer].weightsUpdate + self.momentum * self.layers[ilayer].weightsUpdateSave)
-            self.layers[ilayer].biasesUpdateSave = (-self.alpha * self.layers[ilayer].biasesUpdate + self.momentum * self.layers[ilayer].biasesUpdateSave)
+            self.layers[ilayer].weightsUpdateSave = (
+                -self.alpha * self.layers[ilayer].weightsUpdate
+                + self.momentum * self.layers[ilayer].weightsUpdateSave)
+            self.layers[ilayer].biasesUpdateSave = (
+                -self.alpha * self.layers[ilayer].biasesUpdate
+                + self.momentum * self.layers[ilayer].biasesUpdateSave)
 
-
-    def run(self)->None:
+    def run(self) -> None:
         """
         Train
         """
 
-        for iter in tqdm(range(self.niter), bar_format='{l_bar}{bar:50}{r_bar}{bar:-50b}', desc="Training..."):
+        for iter in tqdm(range(self.niter),
+                         bar_format='{l_bar}{bar:50}{r_bar}{bar:-50b}',
+                         desc="Training..."):
             # Re-initialize update arrays
             self.initializeUpdate()
 
@@ -110,7 +127,7 @@ class GradientDescent:
 
             # Loop over datasets
             for idata in range(self.batchsize):
-                
+
                 # Split dataset
                 inValues, outValues = self.dataSplit(self.dataset[idata])
 
