820157c5671610e58e8e7b05b5d6e60d5a79760a
PageotD <damien.pageot@gmail.com>
2021-09-20 17:39:16 +0200

    Working on Particle Swarm Optimization

---
+++ pybann/particleswarm.py
@@ -0,0 +1,17 @@
+"""
+Particle Swarm Optimization
+"""
+
+from typing import Union
+import numpy as np
+from tqdm import tqdm
+
+class ParticleSwarm:
+    """
+    Class to run Particle Swarm Optimization (PSO) as an optimizer
+    for the artificial neural network
+    """
+
+    def __inti__(self):
+        pass
+    
